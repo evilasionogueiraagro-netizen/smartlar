dacddbca90642b27a10baef462dc2f453f29d25a
evilasionogueiraagro-netizen <evilasionogueira.agro@gmail.com>
2026-04-08 21:56:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
+import os
 from fastapi import FastAPI
+import mysql.connector
 
 app = FastAPI()
+
+def conectar():
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASS"),
+        database=os.getenv("DB_NAME"),
+        port=int(os.getenv("DB_PORT"))
+    )
 
 @app.get("/")
 def home():
     return {"status": "SmartLar online 🚀"}
+
+@app.get("/teste-banco")
+def teste():
+    conn = conectar()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT COUNT(*) FROM usuarios")
+    total = cursor.fetchone()[0]
+
+    cursor.close()
+    conn.close()
+
+    return {"usuarios": total}
