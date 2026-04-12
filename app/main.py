abbe3a8a48d3c88d5d820c623e14254dfb637b3d
evilasionogueiraagro-netizen <evilasionogueira.agro@gmail.com>
2026-04-12 13:44:05 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,3 +8,15 @@ app.include_router(assinatura.router)
 @app.get("/")
 def home():
     return {"status": "SmartLar online 🚀"}
+@app.get("/debug/contratos")
+def listar():
+    conn = get_conn()
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM contratos")
+    dados = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return dados
