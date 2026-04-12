2b43732668f11905edbdcbe5abd8a863c51ac7a1
evilasionogueiraagro-netizen <evilasionogueira.agro@gmail.com>
2026-04-12 13:36:53 -0400

    Update database.py

--- app/db/database.py
+++ app/db/database.py
@@ -1,15 +1,20 @@
 import mysql.connector.pooling
 from app.core.config import settings
 
-pool = mysql.connector.pooling.MySQLConnectionPool(
-    pool_name="smartlar_pool",
-    pool_size=5,
-    host=settings.DB_HOST,
-    user=settings.DB_USER,
-    password=settings.DB_PASS,
-    database=settings.DB_NAME,
-    port=settings.DB_PORT
-)
+pool = None
 
 def get_conn():
+    global pool
+
+    if pool is None:
+        pool = mysql.connector.pooling.MySQLConnectionPool(
+            pool_name="smartlar_pool",
+            pool_size=5,
+            host=settings.DB_HOST,
+            user=settings.DB_USER,
+            password=settings.DB_PASS,
+            database=settings.DB_NAME,
+            port=settings.DB_PORT
+        )
+
     return pool.get_connection()
