d9de78c386996ee8639a783385f5d6fab7117e70
evilasionogueiraagro-netizen <evilasionogueira.agro@gmail.com>
2026-04-12 15:07:06 -0400

    Update inquilinos.py

--- routes/inquilinos.py
+++ routes/inquilinos.py
@@ -1,18 +1,17 @@
 from fastapi import APIRouter
-from app.database import conectar
-from app.models.inquilino import Inquilino
+from app.db.database import get_conn
 
 router = APIRouter()
 
 @router.post("/inquilinos")
-def criar_inquilino(inq: Inquilino):
-    conn = conectar()
+def criar_inquilino(nome: str, cpf: str, telefone: str, residencial_id: int):
+    conn = get_conn()
     cursor = conn.cursor()
 
     cursor.execute("""
         INSERT INTO inquilinos (nome, cpf, telefone, residencial_id)
         VALUES (%s, %s, %s, %s)
-    """, (inq.nome, inq.cpf, inq.telefone, inq.residencial_id))
+    """, (nome, cpf, telefone, residencial_id))
 
     conn.commit()
     cursor.close()
