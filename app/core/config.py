7e8d4d8b707a31b0791232c5fbf9beeccc6cf3ab
evilasionogueiraagro-netizen <evilasionogueira.agro@gmail.com>
2026-04-12 14:25:17 -0400

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Settings:
-    DB_HOST = os.getenv("shinkansen.proxy.rlwy.net")
+    DB_HOST = os.getenv("smartlar-production.up.railway.app")
     DB_USER = os.getenv("root")
     DB_PASS = os.getenv("SVYTByDzVrvqBsrfkEaMpyXnUFzpkzKx")
     DB_NAME = os.getenv("railway")
