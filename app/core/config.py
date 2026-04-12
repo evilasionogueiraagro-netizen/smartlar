02fa506b21166834e71883f1183e430632902082
evilasionogueiraagro-netizen <evilasionogueira.agro@gmail.com>
2026-04-12 12:36:53 -0400

    Create config.py

---
+++ app/core/config.py
@@ -0,0 +1,14 @@
+import os
+
+class Settings:
+    DB_HOST = os.getenv("MYSQLHOST")
+    DB_USER = os.getenv("MYSQLUSER")
+    DB_PASS = os.getenv("MYSQLPASSWORD")
+    DB_NAME = os.getenv("MYSQLDATABASE")
+    DB_PORT = int(os.getenv("MYSQLPORT", 3306))
+
+    ZAPI_INSTANCE = os.getenv("ZAPI_INSTANCE")
+    ZAPI_TOKEN = os.getenv("ZAPI_TOKEN")
+    ZAPI_CLIENT = os.getenv("ZAPI_CLIENT")
+
+settings = Settings()
