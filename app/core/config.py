d254e1bb420bc0ee62ba6ca2c7e9ee5f53a147b2
evilasionogueiraagro-netizen <evilasionogueira.agro@gmail.com>
2026-04-12 14:34:37 -0400

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,11 +1,11 @@
 import os
 
 class Settings:
-    DB_HOST = os.getenv("shinkansen.proxy.rlwy.net")
-    DB_USER = os.getenv("root")
-    DB_PASS = os.getenv("SVYTByDzVrvqBsrfkEaMpyXnUFzpkzKx")
-    DB_NAME = os.getenv("railway")
-    DB_PORT = int(os.getenv("${{MySQL.MYSQLPORT}}", 44842))
+    DB_HOST = os.getenv("MYSQLHOST")
+    DB_USER = os.getenv("MYSQLUSER")
+    DB_PASS = os.getenv("MYSQLPASSWORD")
+    DB_NAME = os.getenv("MYSQLDATABASE")
+    DB_PORT = int(os.getenv("MYSQLPORT", 44842))
 
     ZAPI_INSTANCE = os.getenv("3F1847ADAFDE21F3ABE79ED390C0144C")
     ZAPI_TOKEN = os.getenv("C619984FEC04E144B8F2E9C2")
