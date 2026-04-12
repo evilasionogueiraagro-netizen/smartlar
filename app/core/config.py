dafbeb4fb8130654fd33c5673514a399632d2403
evilasionogueiraagro-netizen <evilasionogueira.agro@gmail.com>
2026-04-12 13:05:30 -0400

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,14 +1,14 @@
 import os
 
 class Settings:
-    DB_HOST = os.getenv("MYSQLHOST")
-    DB_USER = os.getenv("MYSQLUSER")
-    DB_PASS = os.getenv("MYSQLPASSWORD")
-    DB_NAME = os.getenv("MYSQLDATABASE")
-    DB_PORT = int(os.getenv("MYSQLPORT", 3306))
+    DB_HOST = os.getenv("${{MySQL.MYSQLHOST}}")
+    DB_USER = os.getenv("${{MySQL.MYSQLUSER}}")
+    DB_PASS = os.getenv("${{MySQL.MYSQLPASSWORD}}")
+    DB_NAME = os.getenv("${{MySQL.MYSQLDATABASE}}")
+    DB_PORT = int(os.getenv("${{MySQL.MYSQLPORT}}", 3306))
 
-    ZAPI_INSTANCE = os.getenv("ZAPI_INSTANCE")
-    ZAPI_TOKEN = os.getenv("ZAPI_TOKEN")
-    ZAPI_CLIENT = os.getenv("ZAPI_CLIENT")
+    ZAPI_INSTANCE = os.getenv("3F1847ADAFDE21F3ABE79ED390C0144C")
+    ZAPI_TOKEN = os.getenv("C619984FEC04E144B8F2E9C2")
+    ZAPI_CLIENT = os.getenv("F197903852ddf43358ce450b7eea92399S")
 
 settings = Settings()
