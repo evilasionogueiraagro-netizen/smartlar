805ce1b1e65cbbb07184d516f18133bddddbfda0
evilasionogueiraagro-netizen <evilasionogueira.agro@gmail.com>
2026-04-12 14:22:20 -0400

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -5,7 +5,7 @@ class Settings:
     DB_USER = os.getenv("root")
     DB_PASS = os.getenv("SVYTByDzVrvqBsrfkEaMpyXnUFzpkzKx")
     DB_NAME = os.getenv("railway")
-    DB_PORT = int(os.getenv("${{MySQL.MYSQLPORT}}", 3306))
+    DB_PORT = int(os.getenv("${{MySQL.MYSQLPORT}}", 44842))
 
     ZAPI_INSTANCE = os.getenv("3F1847ADAFDE21F3ABE79ED390C0144C")
     ZAPI_TOKEN = os.getenv("C619984FEC04E144B8F2E9C2")
